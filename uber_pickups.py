ca6629b1982a4450341a9d6f35a22f2ca0d6145a
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-19 16:32:40 +0100

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -33,6 +33,7 @@ add_logo("./assets/KI.jpg", height=100)
 pg = st.navigation(
     [
         st.Page(Home),
+	st.Page('pages/8_📄_EDA-ML Reports.py'),    
         st.Page('pages/8_📄_EDA-ML Reports.py'),
 	st.Page('pages/9_📜_Clinical Reports.py')    
     ]
