6dd33f75aa3df5683751d41f7224ce6b30a49ec4
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-19 06:24:09 +0100

    Update 8_📄_EDA-ML Reports.py

--- pages/8_📄_EDA-ML Reports.py
+++ pages/8_📄_EDA-ML Reports.py
@@ -1,1 +1,35 @@
-a
+#https://www.geeksforgeeks.org/how-to-import-local-modules-with-python/
+#EDA-Reports=os.path.join(cwd, "modules/programs/EDA-Reports.py")
+#https://csatlas.com/python-import-file-module/
+
+import streamlit as st  
+import streamlit.components.v1 as components
+import time
+from  PIL import Image
+import numpy as np
+import pandas as pd
+import base64
+import sys
+import inspect, os
+import pathlib
+from os import listdir
+from os.path import isfile, join
+import glob
+import os
+import subprocess
+import modules.ReadPath as m
+
+st.set_page_config(
+    page_title="EDA/ML Reports",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
+m.select_file() 
