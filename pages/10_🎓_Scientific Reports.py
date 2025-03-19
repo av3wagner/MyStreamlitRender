c57873f5f4fb57f296133659fa0fb20adc2e1080
av3wagner <67894780+av3wagner@users.noreply.github.com>
2025-03-19 14:48:11 +0100

    Add files via upload

---
+++ pages/10_🎓_Scientific Reports.py
@@ -0,0 +1,30 @@
+import streamlit as st  
+import streamlit.components.v1 as components
+from  PIL import Image
+import numpy as np
+import pandas as pd
+import base64
+import sys
+import inspect, os
+import pathlib
+from os import listdir
+from os.path import isfile, join
+import glob
+import os
+import subprocess
+import modules.ReadPath as m
+
+st.set_page_config(
+    page_title="Scientific Reports",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
+
+m.open_file_selection_doc()
---
+++ pages/11_🎯_Statistics Samples.py
@@ -0,0 +1,14 @@
+import streamlit as st  
+import streamlit.components.v1 as components
+from  PIL import Image
+import numpy as np
+import pandas as pd
+import base64
+import sys
+import inspect, os
+import pathlib
+from os import listdir
+from os.path import isfile, join
+import glob
+
+st.set_page_config(page_title="Mashine learning Samples")
